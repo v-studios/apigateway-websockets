630acfd3a43bf05c2616fab5693a9d9549d839e4
Paul <pchinjr@gmail.com>
2019-06-13 11:38:29 -0400

    see if it works

--- handler.js
+++ handler.js
@@ -1,5 +1,116 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+let dynamo = new AWS.DynamoDB.DocumentClient();
+
+require('aws-sdk/clients/apigatewaymanagementapi');
+
+const CHATCONNECTION_TABLE = 'chatIdTable';
+
+const successfullResponse = {
+  statusCode: 200,
+  body: 'everything is alright'
+}
+
 module.exports.connectionHandler = (event, context, callback) => {
   console.log(event);
+
+  if (event.requestContext.eventType === 'CONNECT') {
+    //handle connection
+    addConnection(event.requestContext.connectionId)
+      .then(() => {
+        callbback(null, successfullResponse);
+      })
+      .catch(err => {
+        console.log(err);
+        callback(null, JSON.stringify(err));
+      });
+  } else if (event.requestContext.eventType === 'DISCONNECT') {
+    //handle disconnection
+    deleteConnection(event.requestContext.connectionId)
+      .then(() => {
+        callback(null, successfullResponse);
+      })
+      .catch(err => {
+        console.log(err);
+        callback(null, {
+          statusCode: 500,
+          body: 'Failed to connect: ' + JSON.stringify(err)
+        });
+      });
+    }
+}
+
+module.exports.defaultHandler = (event, context, callback) => {
+  console.log('defaultHandler was called');
+  console.log(event);
+
+  callback(null, {
+    statusCode: 200,
+    body: 'defaultHandler'
+  });
+};
+
+module.exports.sendMessageHandler = (event, context, callback) => {
+  sendMessageToAllConnected(event).then(() => {
+    callback(null, successfullResponse);
+  }).catch (err => {
+    callback(null, JSON.stringify(err))
+  });
+}
+
+const sendMessageToAllConnected = (event) => {
+  return getConnectionIds().then((connectionData) => {
+    return connectionData.Items.map(connectionId => {
+      return send(event, connectionId.connectionId);
+    });
+  });
+}
+
+const getConnectionIds = () => {
+  const params = {
+    TableName: CHATCONNECTION_TABLE,
+    ProjecttionExpression: 'connectionId'
+  };
+
+  return dynamo.scan(params).promise();
+}
+
+const send = (event, connectionId) => {
+  const body = JSON.parse(event.body);
+  const postData = body.data;
+
+  const endpoint = event.requestContext.domainName + "/" + event.requestContext.stage;
+  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+    apiVersion: "2018-11-29",
+    endpoint: endpoint
+  });
+
+  const params = {
+    ConnectionId: connectionId,
+    Data: postData
+  };
+  return apigwManagementApi.postToConnection(params).promise();
+}
+
+const addConnection = connectionId => {
+  const params = {
+    TableName: CHATCONNECTION_TABLE,
+    Item: {
+      connectionId: connectionId
+    }
+  };
+
+  return dynamo.put(params).promise();
+}
+
+const deleteConnection = connectionId => {
+  const params = {
+    TableName: CHATCONNECTION_TABLE,
+    Key: {
+      connectionId: connectionId
+    }
+  };
+
+  return dynamo.delete(params).promise();
 };
