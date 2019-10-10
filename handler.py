549553a6bd432e73c192af10581282fac1bc6290
Chris Shenton <chris@shenton.org>
2019-10-10 14:25:01 -0400

    Fix route for sendMessage

--- handler.py
+++ handler.py
@@ -13,68 +13,85 @@ successfullResponse = {
 
 
 def connectionHandler(event, context):
-  # print(f'## connectionHandler event={json.dumps(event)}')
-  if event['requestContext']['eventType'] == 'CONNECT':
-      #handle connection
-      try:
-          cid = event['requestContext']['connectionId']
-          print(f'# addConnetion cid={cid}')
-          addConnection(cid)
-          return successfullResponse
-      except Exception as err:
-        print(f'ERROR failed to addConnection: {err}')
-        # If I raise this will retry, desired?
-        raise RuntimeError(err)
-  elif event['requestContext']['eventType'] == 'DISCONNECT':
-      ## handle disconnection
-      try:
-          deleteConnection(event['requestContext']['connectionId'])
-          return successfullResponse
-      except Exception as err:
-          raise RuntimeError(f'ERROR failed to disconnect: {err}')
+    # print(f'## connectionHandler event={json.dumps(event)}')
+    if event['requestContext']['eventType'] == 'CONNECT':
+        #handle connection
+        try:
+            cid = event['requestContext']['connectionId']
+            print(f'# addConnetion cid={cid}')
+            addConnection(cid)
+            return successfullResponse
+        except Exception as err:
+            print(f'ERROR failed to addConnection: {err}')
+            # If I raise this will retry, desired?
+            raise RuntimeError(err)
+    elif event['requestContext']['eventType'] == 'DISCONNECT':
+        ## handle disconnection
+        try:
+            cid = event['requestContext']['connectionId']
+            print(f'## connectionHandler delete id={cid}')
+            deleteConnection(cid)
+            return successfullResponse
+        except Exception as err:
+            raise RuntimeError(f'ERROR failed to disconnect: {err}')
 
 
 def defaultHandler(event, context):
-  print('defaultHandler was called')
-  print(event);
-  return {'statusCode': 200, body: 'defaultHandler'}
+    print(f'### defaultHandler event={json.dumps(event)}')
+    body = event['body']
+    print(f'### defaultHandler body={json.dumps(body)}')
+    return successfullResponse
 
 
 def sendMessageHandler(event, context):
-  try:
-      sendMessageToAllConnected(event)
-      return successfullResponse
-  except Exception as err:
-      print(f'ERROR sendMessageHandler: {err}')
+    try:
+        print('# sendMessageHandler sending to all...')
+        sendMessageToAllConnected(event)
+        print('# sendMessageHandler rturn success...')
+        return successfullResponse
+    except Exception as err:
+        print(f'ERROR sendMessageHandler: {err}')
 
 
 def sendMessageToAllConnected(event):
-  connectionData = getConnectionIds()
-  for connectionId in collectionData['Items']:
-      send(event, connectionId.connectionId)
+    print('# sendMessageToAllConnected...')
+    connectionData = getConnectionIds()
+    print(f'# sendMessageToAllConnected Items={connectionData["Items"]}')
+    for connectionId in connectionData['Items']:
+        try:                    # we may have stale connections
+            print(f'# sendMessageToAllConnected id={connectionId}')
+            send(event, connectionId.connectionId)
+            print(f'# sendMessageToAllConnected id={connectionId} ok')
+        except Exception as e:
+            print(f'# sendMessageToAllConnected id={connectionId} failed, ignoring')
 
 
 def getConnectionIds():
+    print(f'getConnectionIds...')
     return dynamo.scan(TableName=CHATCONNECTION_TABLE,
-                       ProjecttionExpression='connectionId')
+                       ProjectionExpression='connectionId')
 
 
 def send(event, connectionId) :
-    print(f'## event={event}')
+    print(f'## send event={json.dumps(event)}')
     body = json.loads(event['body']);
+    print(f'## send body=={json.dumps(body, indent=2)}')
     postData = body['data'];
-    # endpoint = event['requestContext']['domainName'] + '/' + event['requestContext']['stage']
-    # AWS.ApiGatewayManagementApi(endpoint=endpoint)
-    apigwManagementApi = boto3.client('apigatewaymanagementapi')
-    return apigwManagementApi.postToConnection(ConnectionId=connectionId,
-                                               Data=postData)
+    print(f'## send postData=={json.dumps(postData, indent=2)}')
+    endpoint = f'https://{event["requestContext"]["domainName"]}/{event["requestContext"]["stage"]}'
+    apigwManagementApi = boto3.client('apigatewaymanagementapi', endpoint=endpoint)
+    print(f'## send apigma=={apigwManagementApi}')
+    return apigwManagementApi.post_to_connection(ConnectionId=connectionId,
+                                                 Data=postData)
 
 
 def addConnection(connectionId):
-  return dynamo.put_item(TableName=CHATCONNECTION_TABLE,
-                         Item={'connectionId': {'S': connectionId}})
+    print(f'addConnection id={connectionId}')
+    return dynamo.put_item(TableName=CHATCONNECTION_TABLE,
+                           Item={'connectionId': {'S': connectionId}})
 
 
 def deleteConnection(connectionId):
+    print(f'deleteConnection id={connectionId}')
     return dynamo.delete_item(TableName=CHATCONNECTION_TABLE,
-                              Key={'connectionId': connectionId})
+                              Key={'connectionId': {'S': connectionId}})
