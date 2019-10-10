19150c7490f1e114211f5f899530691ba55e6222
Chris Shenton <chris@shenton.org>
2019-10-10 14:25:17 -0400

    Connects and sends, but not receiving yet

---
+++ websocket.js
@@ -0,0 +1,32 @@
+// small helper function for selecting element by id
+let id = id => document.getElementById(id);
+
+//Establish the WebSocket connection and set up event handlers
+//let ws = new WebSocket("ws://" + location.hostname + ":" + location.port + "/chat");
+let ws = new WebSocket("wss://u6k9ujsvz7.execute-api.us-east-1.amazonaws.com/dev");
+
+ws.onopen = () => alert("WebSocket opened");
+ws.onclose = () => alert("WebSocket connection closed");
+ws.onmessage = msg => updateChat(msg);
+
+// Add event listeners to button and input field
+id("send").addEventListener("click", () => sendAndClear(id("message").value));
+id("message").addEventListener("keypress", function (e) {
+    if (e.keyCode === 13) { // Send message if enter is pressed in input field
+        sendAndClear(e.target.value);
+    }
+});
+
+function sendAndClear(message) {
+    if (message !== "") {
+        ws.send(JSON.stringify({'route': 'sendMessage', 'data': message}));
+        id("message").value = "";
+    }
+}
+
+function updateChat(msg) { // Update chat-panel and list of connected users
+    let data = JSON.parse(msg.data);
+    //id("chat").insertAdjacentHTML("afterbegin", data.userMessage);
+    id("chat").insertAdjacentHTML("afterbegin", msg.data);
+    //id("userlist").innerHTML = data.userlist.map(user => "<li>" + user + "</li>").join("");
+}
